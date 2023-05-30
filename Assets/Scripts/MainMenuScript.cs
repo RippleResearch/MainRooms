303b40dd20d7088c1524edf045e4f1bc920645e6
Jack Rutherford <jack.rutherford@hope.edu>
2023-05-30 11:07:27 -0400

    Multitouch for the drum pad is working, multiple sounds will play when you touch multiple buttons at once. Add more sounds next, or a visual drumkit which would be cool.

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,31 +1,86 @@
 using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using static UnityEngine.ParticleSystem;
 
 public class MainMenuScript : MonoBehaviour
 {
     public AudioSource[] audios;
     public Button[] buttons;
     public Button button;
+    [SerializeField] GraphicRaycaster m_Raycaster;
+    PointerEventData m_PointerEventData;
+    [SerializeField] EventSystem m_EventSystem;
+    [SerializeField] RectTransform canvasRect;
     // Start is called before the first frame update
     private void Start()
     {
         audios = GetComponentsInChildren<AudioSource>();
         buttons = GetComponentsInChildren<Button>();
+        m_Raycaster = GetComponent<GraphicRaycaster>();
+        //Fetch the Event System from the Scene
+        m_EventSystem = GetComponent<EventSystem>();
     }
     private void Update()
     {
+        for (var i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                m_PointerEventData = new PointerEventData(m_EventSystem);
+                m_PointerEventData.position = Input.GetTouch(i).position;
+                List<RaycastResult> results = new List<RaycastResult>();
+                m_Raycaster.Raycast(m_PointerEventData, results);
+                if (results.Count > 0)
+                {
+                    RaycastResult res = results[0];
+                    for(var j =  0; j < results.Count; j++) 
+                    {
+                        if (results[j].gameObject.name.Contains("Button"))
+                        {
+                            res = results[j];
+                        }
+                    }
+                    if(res.gameObject.GetComponent<BoxCollider>().isTrigger)
+                    {
+                        res.gameObject.GetComponent<AudioSource>().Play();
+                    }
+                }
+            }
 
+               
+            /* RaycastHit hit;
+             // Construct a ray from the current touch coordinates
+             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+             // Create a particle if hit
+             Debug.Log("1");
+             bool psy = Physics.Raycast(ray, out hit);
+             Debug.Log(psy);
+             if (Physics.Raycast(ray, out hit))
+             {
+                 Debug.Log("2");
+                 if (hit.collider.isTrigger)
+                 {
+                     Debug.Log("3");
+                     hit.collider.GetComponent<AudioSource>().Play();
+                 }
+             }*/
+        }
     }
     public void playButton()
     {
-        /*Debug.Log("Name: " + EventSystem.current.currentSelectedGameObject.name);*/
-        int index = findIndex(buttons, EventSystem.current.currentSelectedGameObject.name);
-        /*Debug.Log("Audio " + index + ": " + audios[index]);*/
-        audios[index].Play();
+        
+    }
+    
+    public void click(int index)
+    {
+        
+        /*collision.GetComponent<AudioSource>().Play();
+        Debug.Log("Comp Name: " + collision.gameObject.name);*/
     }
 
     public void stopPlaying()
