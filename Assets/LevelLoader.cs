743ebb4639bee49734b7a45464d395e766b81c72
Jack Rutherford <jack.rutherford@hope.edu>
2023-06-02 09:47:10 -0400

    Island now serves as a trigger to load a new scene. Fixed rendering issues as well for the impossible cube

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
+    public GameObject mainShip;
+    public TMP_Text txt;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Assert(mainShip != null);
+        Debug.Assert(txt != null);
     }
 
     // Update is called once per frame
@@ -18,6 +23,11 @@ public class LevelLoader : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        string level = txt.text;
+        Debug.Log(level);
+        if (other.gameObject.Equals(mainShip))
+        {
+            SceneManager.LoadScene(level);
+        }
     }
 }
