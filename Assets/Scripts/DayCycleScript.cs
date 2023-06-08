a2623b654c237f734614e49d6a550c741ed63160
Jack Rutherford <jack.rutherford@hope.edu>
2023-06-08 13:48:34 -0400

    Night day transition looking better

--- Assets/Scripts/DayCycleScript.cs
+++ Assets/Scripts/DayCycleScript.cs
@@ -18,6 +18,7 @@ public class DayCycleScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        mat1.SetFloat("_Blend", 1f);
         // How much time in minutes it will take for the sun to rotate 360 degrees
         rotationSpeed = 360 / dayLengthMinutes / 60;
         midday = dayLengthMinutes * 60 / 2;
@@ -68,18 +69,40 @@ public class DayCycleScript : MonoBehaviour
             displayMinutes = "0"+minutes.ToString();
         }
 
-        if (currentTime%(midday*2) >= midday /*&& currentTime <= midday * 1.5f*/)
+        float fadeRate = 0.05f;
+
+        if ((currentTime%(midday*2) >= midday*1.14f) && (currentTime%(midday*2) <= ((midday*2)*0.91f)))
         {
             RenderSettings.skybox = mat1;
+            if(RenderSettings.skybox.GetFloat("_Blend") > 0)
+            {
+                RenderSettings.skybox.SetFloat("_Blend", RenderSettings.skybox.GetFloat("_Blend") - fadeRate);
+            }
+            else
+            {
+                RenderSettings.skybox.SetFloat("_Blend", 0);
+            }
+        }
+        else if (currentTime % (midday * 2) > ((midday * 2) * 0.91f) && currentTime % (midday * 2) <= ((midday * 2) * 0.94f))
+        {
+            if(RenderSettings.skybox.GetFloat("_Blend") < 1)
+            {
+                RenderSettings.skybox.SetFloat("_Blend", RenderSettings.skybox.GetFloat("_Blend") + fadeRate);
+            }
+            else
+            {
+                RenderSettings.skybox.SetFloat("_Blend", 1);
+            }
         }
         else
         {
             RenderSettings.skybox = mat2;
+            
         }
 
         string displayTime = displayHours + ":" + displayMinutes;
         //timeText.text = displayTime;
-        Debug.Log(displayTime);
+        /*Debug.Log(displayTime);*/
 
         this.transform.Rotate(new Vector3(1, 0, 0) * rotationSpeed * Time.deltaTime);
         //transform.RotateAround(this.transform.position, Vector3.right, Time.deltaTime * rotationSpeed);
