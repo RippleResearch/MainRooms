0542724cd01ad2e6b42bbcda350f2bbb240c119f
Jack Rutherford <jack.rutherford@hope.edu>
2023-06-08 11:20:16 -0400

    Basic night and day cycle switch at 6am and 6pm

--- Assets/Scripts/DayCycleScript.cs
+++ Assets/Scripts/DayCycleScript.cs
@@ -7,11 +7,12 @@ public class DayCycleScript : MonoBehaviour
 {
     public float currentTime;
     public float dayLengthMinutes;
-    public Material mat;
+    public Material mat1;
+    public Material mat2;
     private float rotationSpeed;
     //private TMP_Text timeText;
-    private string AMPM = "AM";
-    private float midday;
+    [SerializeField] private string AMPM = "AM";
+    [SerializeField] private float midday;
     private float translateTime;
     
     // Start is called before the first frame update
@@ -25,24 +26,25 @@ public class DayCycleScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotationSpeed);
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotationSpeed /8);
         currentTime += 1 * Time.deltaTime;
         translateTime = (currentTime / (midday * 2));
 
-        float t = translateTime * 24f;
+        float t = 6 + translateTime * 24f;
 
         float hours = Mathf.Floor(t);
 
         string displayHours = hours.ToString();
-        if(hours == 0)
+        /*if(hours == 0)
         {
             displayHours = "12";
-        }
-        if(hours > 12)
+        }*/
+        if (hours > 24)
         {
-            displayHours = (hours - 12).ToString();
+            displayHours = (hours - 24).ToString();
         }
-        if(currentTime >= midday)
+
+        /*if(currentTime >= midday)
         {
             if (!AMPM.Equals("PM"))
             {
@@ -56,7 +58,7 @@ public class DayCycleScript : MonoBehaviour
             {
                 AMPM = "AM";
             }
-        }
+        }*/
 
         t *= 60;
         float minutes = Mathf.Floor(t % 60);
@@ -66,9 +68,18 @@ public class DayCycleScript : MonoBehaviour
             displayMinutes = "0"+minutes.ToString();
         }
 
+        if (currentTime%(midday*2) >= midday /*&& currentTime <= midday * 1.5f*/)
+        {
+            RenderSettings.skybox = mat1;
+        }
+        else
+        {
+            RenderSettings.skybox = mat2;
+        }
 
-        string displayTime = displayHours + ":" + displayMinutes + " " + AMPM;
+        string displayTime = displayHours + ":" + displayMinutes;
         //timeText.text = displayTime;
+        Debug.Log(displayTime);
 
         this.transform.Rotate(new Vector3(1, 0, 0) * rotationSpeed * Time.deltaTime);
         //transform.RotateAround(this.transform.position, Vector3.right, Time.deltaTime * rotationSpeed);
