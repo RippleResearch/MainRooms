5b04c4402d25b9c76218074890afeaa1048ff382
Jack Rutherford <jack.rutherford@hope.edu>
2023-06-13 15:41:26 -0400

    Fixed script bugs and made the level loader to return to main menu. Moved some assets around.

--- Assets/DrumKit/Scripts/DrumkitLevelLoader.cs
+++ Assets/DrumKit/Scripts/DrumkitLevelLoader.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class DrumkitLevelLoader : MonoBehaviour
+public class DrumkitLevelLoader : LevelLoader
 {
+    [SerializeField] private TMP_Text txt;
+    [SerializeField] private GameObject triggerObject;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,29 @@ public class DrumkitLevelLoader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckHit();
+    }
+
+    public override void CheckHit()
+    {
+        // GetMouseButtonDown == (Input.touch.phase == begin)
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // Check to make sure raycasting is only in the right scenes
+            /*Debug.Log(SceneManager.GetActiveScene().name);*/
+            if (Physics.Raycast(ray, out hit))
+            {
+                ray.origin = Camera.main.transform.position;
+                // Check if the thing the ray is hitting is a trigger, and equals 
+                if (hit.collider.isTrigger && hit.collider.gameObject.Equals(triggerObject))
+                {
+                    // Child object of the trigger object is a text TMP, string value of that is the level name
+                    string level = txt.text;
+                    SceneManager.LoadScene(level);
+                }
+            }
+        }
     }
 }
