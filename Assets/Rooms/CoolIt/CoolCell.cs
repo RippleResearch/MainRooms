5ac3a1665e655bddb2d9cd182c1c08d65917551b
ferzle <cusack@hope.edu>
2023-07-06 16:58:41 -0400

    Addedf Cool It

---
+++ Assets/Rooms/CoolIt/CoolCell.cs
@@ -0,0 +1,34 @@
+using System.Text;
+using UnityEngine;
+
+public class CoolCell
+{
+    public CoolBlock Block1;
+    public CoolBlock Block2;
+    public bool isActive = true;
+
+    public CoolCell(CoolBlock block) {
+        Block1 = block;
+    }
+
+    public void SwapBlocksAndVars() {
+        CoolBlock temp = Block1;
+        Block1 = Block2;
+        Block2 = temp;
+
+        Block2.increment = Block1.increment;
+        Block2.Orientation = -Block1.Orientation;
+    }
+
+    public override string ToString() {
+        StringBuilder sb = new();
+        if (Block1 != null)sb.Append("Block1: " + Block1.gameObject.name + " ");
+        else sb.Append("Block1 is null ");
+        
+        if(Block2 != null) sb.Append("Block2: " + Block2.gameObject.name + " ");
+        else sb.Append("Block2 is null ");
+        
+        return sb.ToString();
+    }
+
+}
